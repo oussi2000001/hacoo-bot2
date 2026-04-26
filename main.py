8e1a606638c1920da550e556601ef9f40ef0adac
oussi2000001 <oussamakhalloufi0@gmail.com>
2026-04-26 02:31:49 +0200

    main.py

--- main.py
+++ main.py
@@ -258,4 +258,4 @@ def health():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, threaded=True)
+    app.run(host="0.0.0.0", port=port, threaded=False)
